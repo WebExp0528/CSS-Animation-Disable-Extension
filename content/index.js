9646c3266b57498e8c9a734eeaf70d3e31b7b39f
WebExp0528 <app.dev0528@gmail.com>
2020-06-15 22:12:26 -0700

    Add enable/disable function

--- content/index.js
+++ content/index.js
@@ -1,13 +1,44 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+var EXTENSION_STYLESHEET_ID = "extension_stylesheet";
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+function loadCSS() {
+    var link = document.createElement("link");
+    link.href = chrome.extension.getURL("assets/css/content.css");
+    link.id = EXTENSION_STYLESHEET_ID;
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
 
-	}
-	}, 10);
-});
+function unloadCSS() {
+    var cssNode = document.getElementById(EXTENSION_STYLESHEET_ID);
+    cssNode && cssNode.parentNode.removeChild(cssNode);
+}
+
+function validateCSS() {
+    var cssNode = document.getElementById(EXTENSION_STYLESHEET_ID);
+    console.log("~~~~~~~~~~~~~~~~~~ cssnode", cssNode);
+    return cssNode ? true : false;
+}
+
+function onRequest(message, sender, reply) {
+    switch (message.type) {
+        case "VALIDATE_CSS": {
+            reply(validateCSS());
+            break;
+        }
+        case "LOAD_CSS": {
+            loadCSS();
+            reply(true);
+            break;
+        }
+        case "UNLOAD_CSS": {
+            unloadCSS();
+            reply(true);
+            break;
+        }
+    }
+    return true;
+}
+
+loadCSS();
+chrome.runtime.onMessage.addListener(onRequest);
